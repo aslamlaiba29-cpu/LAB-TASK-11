43b9d5126a3ab7ef45e9c62dac19941bec2229ed
aslamlaiba29-cpu <aslam.laiba29@gmail.com>
2026-04-15 09:37:59 +0500

    neww feature added

---
+++ branches.py
@@ -0,0 +1,16 @@
+# def my_function():
+#     x = 10
+#     return x  # Send the value back to the caller
+#
+# # Save the returned value into a new variable 'x'
+# x = my_function()
+#
+# print(x) # Output: 10
+# local variable''''''
+
+def greet(name):
+
+    print(f"Morning, {name}!")
+
+
+greet("Laiba")
