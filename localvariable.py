fde42e77d6d5625d2e63d35771d61d19f54b962e
aslamlaiba29-cpu <aslam.laiba29@gmail.com>
2026-04-15 09:54:50 +0500

    neww feature added localvariable.py

--- localvariable.py
+++ localvariable.py
@@ -1,16 +1,8 @@
 def my_function():
-    x = 10
+    x = 100
     return x  # Send the value back to the caller
 
 # Save the returned value into a new variable 'x'
 x = my_function()
 
 print(x) # Output: 10
-# local variable''''''
-
-def greet(name):
-
-    print(f"Morning, {name}!")
-
-
-greet("Laiba")
